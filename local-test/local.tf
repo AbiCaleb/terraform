97b1401b84972c0e264a0f7566d1f8989253c271
Abi <abi.m@m2pfintech.com>
2026-03-17 16:21:11 +0530

    testing simple local file block in tf

---
+++ local-test/local.tf
@@ -0,0 +1,5 @@
+resource "local_file" "about_me" {
+    filename = "Documents/tf-testing/about_me.txt"
+    content = "Hello, This is Abi Caleb and Iam  DevOps Engineer at M2PFintech and Iam learning Terraform"
+    file_permission = "0700"
+}
