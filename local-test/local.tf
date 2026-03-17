1e14aee637d425072a344b27a81da1750cf1afa6
Abi <abi.m@m2pfintech.com>
2026-03-17 16:41:33 +0530

    testing random pet provider

--- local-test/local.tf
+++ local-test/local.tf
@@ -3,3 +3,8 @@ resource "local_file" "about_me" {
     content = "Hello, This is Abi Caleb and Iam  DevOps Engineer at M2PFintech and Iam learning Terraform"
     file_permission = "0700"
 }
+resource "random_pet" "pets" {
+    prefix = "Mr"
+    separator = "."
+    length = "1"
+}
